a323e26e1576bf18f8e1bf9c66a157a1200f98b1
imanolazpiroz17 <imanolazpiroz77@gmail.com>
2025-09-25 19:43:28 -0300

    Create ejercicio2.java

---
+++ Practica/AYED/src/tp1/ejercicio2/ejercicio2.java
@@ -0,0 +1,27 @@
+package tp1.ejercicio2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class ejercicio2 {
+
+    public static int[] arregloDeMultiplos(int n){
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++){ 
+            a[i] = (n * (i + 1));
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        
+        Scanner s = new Scanner(System.in);
+        
+        int n = s.nextInt();
+        while(n != 0){
+            System.out.println(Arrays.toString(arregloDeMultiplos(n)));
+            n = s.nextInt();
+        }
+    }
+}
